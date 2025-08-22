b35d0c061b6ca9aeec7c201be3955b56aa78bb71
Sanjana Checker <schecker@nvidia.com>
2025-08-22 12:00:15 -0700

    fixed backend port

--- examples/factory_reset/ui/backend/main.py
+++ examples/factory_reset/ui/backend/main.py
@@ -320,4 +320,4 @@ if static_path.exists():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, log_level="info")
